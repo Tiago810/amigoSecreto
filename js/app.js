b4e93cdab3e4388b0a59ca1f3334830ef2423af0
Tiago Campos <tiagocampos810@gmail.com>
2025-04-13 21:51:38 -0300

    sorteio unico

---
+++ js/app.js
@@ -0,0 +1,33 @@
+let incluidos = [];
+let disponiveis = [];
+let lista = document.getElementById('lista-amigos');
+let campoSorteio = document.getElementById('lista-sorteio');
+function adicionar() {
+    let nome = document.getElementById('nome-amigo').value.trim();
+    if (nome && !incluidos.includes(nome)) {
+        incluidos.push(nome);
+        disponiveis.push(nome);
+        lista.textContent = incluidos.join(', ');
+        document.getElementById('nome-amigo').value = ('');
+    } else {
+        alert("Nome inválido ou já incluido");
+    }
+}
+
+function sortear() {
+    let quantidade = disponiveis.length;
+    if (quantidade > 0) {
+        let sorteio = parseInt(Math.random() * quantidade);
+        let amigoSorteado = disponiveis[sorteio];
+        campoSorteio.textContent = amigoSorteado;
+        disponiveis.splice(sorteio,1);
+    } else {
+        alert("Nenhum nome disponível");
+    }
+}
+
+function reiniciar() {
+    incluidos = [];
+    disponiveis = [];
+    lista.textContent = ('');
+}
